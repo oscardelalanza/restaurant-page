f503e08d5b93312c836693871486c3cfab8f3048
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-01-09 22:50:27 -0800

    tab events moved to index.js

--- src/header.js
+++ src/header.js
@@ -1,52 +1,21 @@
-import { home } from './home';
-import { footer } from './footer';
-import { menu, gallery } from './menu';
-import { location, map } from './contact';
+const homeLink = document.createElement('a');
+const menuLink = document.createElement('a');
+const contactLink = document.createElement('a');
 
 // navigation menu
-export const nav = () => {
+const nav = () => {
 
     const header = document.createElement('header');
     const navTag = document.createElement('nav');
-    const homeLink = document.createElement('a');
-    const menuLink = document.createElement('a');
-    const contactLink = document.createElement('a');
-    const content = document.getElementById('content');
 
     homeLink.innerText = 'Home';
     homeLink.href = '#';
-    homeLink.addEventListener('click', () => {
-
-        content.innerHTML = '';
-        content.appendChild(nav());
-        content.appendChild(home());
-        content.appendChild(footer());
-
-    });
 
     menuLink.innerText = 'Menu';
     menuLink.href = '#';
-    menuLink.addEventListener('click', () => {
-
-        content.innerHTML = '';
-        content.appendChild(nav());
-        content.appendChild(menu());
-        content.appendChild(gallery());
-        content.appendChild(footer());
-
-    });
 
     contactLink.innerText = 'Contact Us';
     contactLink.href = '#';
-    contactLink.addEventListener('click', () => {
-
-        content.innerHTML = '';
-        content.appendChild(nav());
-        content.appendChild(location());
-        content.appendChild(map());
-        content.appendChild(footer());
-
-    });
 
     navTag.appendChild(homeLink);
     navTag.appendChild(menuLink);
@@ -57,3 +26,12 @@ export const nav = () => {
     return header;
 
 };
+
+export {
+
+    homeLink,
+    menuLink,
+    contactLink,
+    nav,
+
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { nav } from './header';
+import { homeLink, menuLink, contactLink, nav } from './header';
 import { home } from './home';
+import { menu, gallery } from './menu';
+import { location, map } from './contact';
 import { footer } from './footer';
 
 const content = document.getElementById('content');
@@ -7,3 +9,32 @@ const content = document.getElementById('content');
 content.appendChild(nav());
 content.appendChild(home());
 content.appendChild(footer());
+
+homeLink.addEventListener('click', () => {
+
+    content.innerHTML = '';
+    content.appendChild(nav());
+    content.appendChild(home());
+    content.appendChild(footer());
+
+});
+
+menuLink.addEventListener('click', () => {
+
+    content.innerHTML = '';
+    content.appendChild(nav());
+    content.appendChild(menu());
+    content.appendChild(gallery());
+    content.appendChild(footer());
+
+});
+
+contactLink.addEventListener('click', () => {
+
+    content.innerHTML = '';
+    content.appendChild(nav());
+    content.appendChild(location());
+    content.appendChild(map());
+    content.appendChild(footer());
+
+});
