f9fbf0c9dd4283ec99ecc898d7a8912dfc4d8947
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-01-12 14:39:56 -0800

    home styles added

--- src/home.js
+++ src/home.js
@@ -9,27 +9,28 @@ export const home = () => {
     const description = document.createElement('p');
     const imgContainer = document.createElement('div');
     const img = document.createElement('img');
+    const aboutContainer = document.createElement('div');
 
-    // TODO: continue here
-    // TODO: add about container
+    img.src = About;
+    imgContainer.className = 'img-container';
+    imgContainer.appendChild(img);
 
-    title.innerText = 'About The Coffee Shop';
+    title.innerText = 'About Us';
     subtitle.innerText = 'Here should be the description but instead of that here is just a random text :)';
     description.innerText = 'Morbi euismod lacus arcu, volutpat rutrum lacus vulputate quis. Suspendisse eget risus ' +
         'luctus eros aliquet vestibulum vel nec sem. Aenean facilisis elementum dolor, sed lobortis eros lacinia vel. ' +
         'Mauris nec quam eu metus porttitor pharetra tincidunt a lacus. Donec sit amet viverra augue. Aenean nunc ' +
         'sapien, posuere nec suscipit non, convallis at metus. Sed accumsan fringilla diam. Sed tempus, diam ut rutrum ' +
         'accumsan, leo dolor pharetra mauris, non scelerisque quam est id velit.';
-    img.src = About;
-    imgContainer.className = 'img-container';
+    aboutContainer.className = 'about-container';
+    aboutContainer.appendChild(title);
+    aboutContainer.appendChild(subtitle);
+    aboutContainer.appendChild(description);
+
     home.classList.add('container');
     home.classList.add('home');
-
-    imgContainer.appendChild(img);
     home.appendChild(imgContainer);
-    home.appendChild(title);
-    home.appendChild(subtitle);
-    home.appendChild(description);
+    home.appendChild(aboutContainer);
 
     return home;
 
