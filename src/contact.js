7e67dfc73ff71c8b51a221b23fe123a3bcf5a11f
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-01-12 16:47:40 -0800

    map styles added

--- src/contact.js
+++ src/contact.js
@@ -48,6 +48,8 @@ const map = () => {
 
     const map = document.createElement('iframe');
 
+    map.classList.add('map');
+
     map.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d338.3397555421225!2d151.21314029242754!3d-33.85956641319514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12ae67d5541c77%3A0x8af817cc7e3857d7!2sAria%20Restaurant%20Sydney!5e0!3m2!1ses!2smx!4v1578622220577!5m2!1ses!2smx';
     map.width = '800';
     map.height = '600';
