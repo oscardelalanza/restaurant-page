a6809a2be60ba4960247c7a66688d2d2e738a55e
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-01-13 12:38:19 -0800

    webpack scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
+    devServer: {
+        contentBase: './dist',
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
